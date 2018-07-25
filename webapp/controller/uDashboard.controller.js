f252af98713294378fa2eea1e9852af67afa0014
Catalin-Vlad Mercea <catalin-vlad.mercea@mhp.com>
2018-07-25 10:48:19 +0000

    User/Admin Dashboard

---
+++ webapp/controller/uDashboard.controller.js
@@ -0,0 +1,9 @@
+sap.ui.define([
+	"eventManagementEVA/controller/BaseController"
+], function(BaseController) {
+	"use strict";
+
+	return BaseController.extend("eventManagementEVA.controller.uDashboard", {
+	});
+
+});
