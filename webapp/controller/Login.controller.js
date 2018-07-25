414cc0c50213f932581754b1d4dad128dba8cc39
Opre Alexandru <alexandru.opre@mhp.com>
2018-07-25 12:47:36 +0000

    Changes 4

--- webapp/controller/Login.controller.js
+++ webapp/controller/Login.controller.js
@@ -10,6 +10,21 @@ sap.ui.define([
 		},
 		userDashboard: function() {
 			this.getRouter().navTo("uDashboard");
+		},
+		onSavePress: function(oEvent) {
+			var oView = this.getView();
+			var oModel = oView.getModel();
+			oModel.setUseBatch("true");
+			oModel.submitChanges({
+				success: jQuery.proxy(function() {
+					new sap.m.MessageToast.show("Changes successfully saved!");
+				}, this),
+				error: jQuery.proxy(function() {
+					oModel.setUseBatch(false);
+					new sap.m.MessageToast.show("Error saving changes!");
+
+				}, this)
+			});
 		}
 	});
 
