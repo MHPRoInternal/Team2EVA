80f300d3d74acfbd44d6dc1d27e99f38e22b84b9
Opre Alexandru <alexandru.opre@mhp.com>
2018-07-27 06:14:43 +0000

    Latest Changes (PLEASE DELETE THE POPUP IF IT ANNOYS YOU, AS IT IS NOT CURRENTLY WORKING)

--- webapp/controller/Login.controller.js
+++ webapp/controller/Login.controller.js
@@ -1,42 +1,70 @@
 sap.ui.define([
-	"eventManagementEVA/controller/BaseController"
-], function(BaseController) {
+	"eventManagementEVA/controller/BaseController",
+	'sap/ui/core/Popup',
+	'sap/m/Button'
+], function(BaseController, Popup, Button) {
 	"use strict";
 
 	return BaseController.extend("eventManagementEVA.controller.Login", {
 
-		onLoginTap: function() {
-			var oView = this.getView();
-			var oModel = oView.getModel();
-			var uid = this.getView().byId("uid").getValue();
-			var pasw = this.getView().byId("pasw").getValue();
-			var route = this.getRouter();
-
-			oModel.read("/UserSet(IdUser='" + uid + "',Password='" + pasw + "')", {
-				success: function(oCompleteEntry) {
-					if (oCompleteEntry.Role === true) {
-						route.navTo("aDashboard");
-					} else if(oCompleteEntry.Role === false) {
-						route.navTo("uDashboard");
 				
+		onLoginTap: function(oEvent) {
+				var oView = this.getView();
+				var oModel = oView.getModel();
+				var uid = this.getView().byId("uid").getValue();
+				var pasw = this.getView().byId("pasw").getValue();
+				var route = this.getRouter();
+				//var oListItem = oEvent.getSource();
+				// var oBindingContext = oListItem.getBindingContext();
+				// var userId = oBindingContext.getObject().IdUser;
+				//var loginSuccess = "Login Successful! Welcome, " + uid + ".";
+				
+				// var oPopUp = new Popup({
+				// 			modal: true,
+
+				// 			content: new Text({
+				// 				text: loginSuccess
+				// 			}),
+							
+				// 			autoclose: true
+
+				// 		});
+				// oPopUp.open();
+						
+				oModel.read("/UserSet(IdUser='" + uid + "',Password='" + pasw + "')", {
+					success: function(oCompleteEntry) {
+						if (oCompleteEntry.Role === true) {
+						
+							route.navTo("aDashboard");
+							
+						} else if (oCompleteEntry.Role === false) {
+							
+							route.navTo("uDashboard"
+							, {
+								
+								userID: uid
+							
+							});
+							
+						}
+						
+						
+					},
+					error: function(oError) {
+						console.log("Not very nice!");
 					}
-				},
-				error: function(oError) {
-					console.log("Not very nice!");
-				}
-				
-			});
-			
 
-		}
-		// 	var oModel = new sap.ui.model.odata.v2.ODataModel({
-		// 		serviceUrl: "http://services.odata.org/Northwind/Northwind.svc",
-		// 		metadataUrlParams: {
-		// 			ID_USER: "{uid}",
-		// 			PASSWORD: "{pasw}"
-		// 		}
-		// 	});
-		// 
+				});
+
+			}
+			// 	var oModel = new sap.ui.model.odata.v2.ODataModel({
+			// 		serviceUrl: "http://services.odata.org/Northwind/Northwind.svc",
+			// 		metadataUrlParams: {
+			// 			ID_USER: "{uid}",
+			// 			PASSWORD: "{pasw}"
+			// 		}
+			// 	});
+			// 
 	});
 
 });
--- webapp/controller/uDashboard.controller.js
+++ webapp/controller/uDashboard.controller.js
@@ -1,9 +1,39 @@
 sap.ui.define([
-	"eventManagementEVA/controller/BaseController"
-], function(BaseController) {
+	"eventManagementEVA/controller/BaseController",
+	'sap/ui/core/Popup',
+	'sap/m/Button'
+], function(BaseController, Button, Popup) {
 	"use strict";
 
 	return BaseController.extend("eventManagementEVA.controller.uDashboard", {
-	});
 
+		
+		onInit: function(oEvent) {
+			this.getRouter().getRoute("uDashboard").attachMatched(this.onRouteMatched, this);
+			// var oPopUp = new Popup({
+			// 				modal: true,
+
+			// 				content: new Text({
+			// 					text: loginSuccess
+			// 				}),
+							
+			// 				autoclose: true
+
+			// 			});
+			// 			oPopUp.open();
+		},
+
+		onRouteMatched: function(oEvent) {
+			var uID = oEvent.getParameter("arguments").userID;
+			console.log(uID);
+			var oView = this.getView();
+			var oModel = oView.getModel();
+			
+			
+		}
+		
+		
+		
+		
+	});
 });
